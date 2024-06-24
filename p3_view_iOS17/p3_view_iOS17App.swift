633a06faa3fd140d13efe1ca38674c27714dab35
Manjil Rajbhandari <manjilrajbhandari@gmail.com>
2024-06-24 07:01:16 +0545

    Initial Commit

---
+++ p3_view_iOS17/p3_view_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  p3_view_iOS17App.swift
+//  p3_view_iOS17
+//
+//  Created by manjil rajbhandari on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct p3_view_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
